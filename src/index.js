a545f250fc68321a624a3ce4c6b0560bdf2287d1
mehul patel <mehulptl93@gmail.com>
2018-07-27 10:52:02 +0530

    Configuration changes

--- src/index.js
+++ src/index.js
@@ -4,5 +4,9 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import './assets/scss/index.scss';
+
+// require('./assets/scss/index.scss');
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
